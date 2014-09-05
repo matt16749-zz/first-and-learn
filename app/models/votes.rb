fda65282c412f8d5d0df4e425cf47da8ba65a0e8
Gunnari Auvinen & Patrick steps-out-for-coffee-on-the-reg Menzel ;) <pair+cheerazar.pmmenzel@devbootcamp.com>
2014-09-04 20:23:45 -0700

    Update Vote model validations for the appropriate table.

--- app/models/votes.rb
+++ app/models/votes.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
 
-  validates :type, presence: true
+  validates :user_id, :vote_state, :voteable_type, :voteable_id, presence: true
 end
