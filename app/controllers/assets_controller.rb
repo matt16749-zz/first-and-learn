af7c00d7f0facec9afbd4452b3d6cb62da14b5fa
Gunnari Auvinen <apprentice@master-inf-7.local>
2014-09-05 19:22:58 -0700

    Define new action in AssetsController.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -3,6 +3,10 @@ class AssetsController < ApplicationController
     @assets = Asset.all
   end
 
+  def new
+    @asset = Asset.new
+  end
+
   private
   def asset_params
     params.require(:asset).permit(:title, :description, :url)
