65df39b2dd15c00f82c9cf54fe21f0bf87ad8cb1
Tommy Dugger <tommy.dugger78@gmail.com>
2014-09-06 12:09:09 -0700

    changes controller test for editing an asset

--- spec/controllers/assets_spec.rb
+++ spec/controllers/assets_spec.rb
@@ -67,10 +67,10 @@ RSpec.describe AssetsController, :type => :controller do
       expect(response).to have_http_status(302)
     end
 
-    it 'responds successfully with an HTTP 302 status code if user is not logged in' do
+    it 'redirects to login page if user is not logged in' do
       asset = create(:asset)
       post :update, id: asset.id, asset: FactoryGirl.attributes_for(:asset)
-      expect(response).to have_http_status(302)
+      expect(response).to redirect_to '/users/sign_in'
     end
   end
 
