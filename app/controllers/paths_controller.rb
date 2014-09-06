560505d80fa8d6f72708928779e94ec51a11d013
Matt Chan & Patrick steps-out-for-coffee-on-the-reg Menzel ;) <pair+matt16749.pmmenzel@devbootcamp.com>
2014-09-05 18:57:30 -0700

    add @path instance variable to paths#index

---
+++ app/controllers/paths_controller.rb
@@ -0,0 +1,23 @@
+class PathsController < ApplicationController
+  def index
+    @path = Path.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
