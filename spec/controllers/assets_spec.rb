07e5e35f58f438b5e6613d1507d40fc75cc95870
Gunnari Auvinen <apprentice@master-inf-7.local>
2014-09-05 21:31:33 -0700

    Add in test 302 status if user isn't logged in.

--- spec/controllers/assets_spec.rb
+++ spec/controllers/assets_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe AssetsController, :type => :controller do
+  let(:user) { create(:user) }
+
   before :each do
     User.destroy_all
     Asset.destroy_all
@@ -15,8 +17,16 @@ RSpec.describe AssetsController, :type => :controller do
 
   describe 'GET #new' do
     it 'responds successfully with an HTTP 200 status code' do
+      sign_in :user, user
       get :new
       expect(response).to have_http_status(200)
     end
+
+    it "responds with a HTTP 302 status if user isn't logged in" do
+      get :new
+      expect(response).to have_http_status(302)
+    end
+  end
+
   end
 end
