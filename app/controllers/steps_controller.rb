4a39110c741f2932fd54156476b8ec78d374ad6d
Tommy Dugger & Gunnari Auvinen <pair+cerinman.cheerazar@devbootcamp.com>
2014-09-06 17:03:39 -0700

    Added new action in steps controller. Added steps_params method to handle mass assignment.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,3 +1,14 @@
 class StepsController < ApplicationController
 
+  def new
+    redirect_to new_path_step_path unless user_signed_in?
+    @step = Step.new
+    @path = Path.find(params[:path_id])
+    @assets = Asset.where('user_id = ?', current_user.id)
+  end
+
+  private
+  def steps_params
+    params.require(:step).permit(:body, :position)
+  end
 end
