9ecaac9ffe0008dc0022effff54609272d58dad1
Matt Chan & Patrick steps-out-for-coffee-on-the-reg Menzel ;) <pair+matt16749.pmmenzel@devbootcamp.com>
2014-09-06 17:45:50 -0700

    Add Comments#index, Comments#show, Comments#new, Comments#create, Comments#destroy, and private method find_commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,47 @@
 class CommentsController < ApplicationController
-
-  def new
-    path = Path.find(params[:path_id])
-    asset = Asset.find(params[:])
+  def index
+    @commentable = find_commentable
+    @comments = @commentable.comments
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:notice] = "Successfully created comment."
+      redirect_to :id => nil
+    else
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    redirect_to user_session_path and return unless user_signed_in?
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to path_comments_path
+  end
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:id, :body, :user_id, :commentable_type, :commentable_id)
+  end
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
   end
 end
