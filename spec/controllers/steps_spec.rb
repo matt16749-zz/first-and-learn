823b8ca05bdedac35cf7076d3f22313d47845183
Tommy Dugger & Gunnari Auvinen <pair+cerinman.cheerazar@devbootcamp.com>
2014-09-07 12:04:45 -0700

    Add test to steps_spec to verify status 200 for new action, for a logged in user.

--- spec/controllers/steps_spec.rb
+++ spec/controllers/steps_spec.rb
@@ -32,4 +32,12 @@ RSpec.describe StepsController, :type => :controller do
       expect(response).to redirect_to '/users/sign_in'
     end
   end
+
+  describe "GET #new" do
+    it 'responds successfully with an HTTP 200 status code' do
+      sign_in :user, user
+      get :new, path_id: path.id, id: step.id
+      expect(response).to have_http_status(200)
+    end
+  end
 end
