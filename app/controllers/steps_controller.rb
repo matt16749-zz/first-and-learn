32e102becfb6d83a0c8afedbee74dd01f1cf576c
Tommy Dugger & Gunnari Auvinen <pair+cerinman.cheerazar@devbootcamp.com>
2014-09-06 19:36:52 -0700

    Add steps create action. Add before_validation callback to update position number. Add get_position method in the Step model.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -7,6 +7,18 @@ class StepsController < ApplicationController
     @assets = Asset.where('user_id = ?', current_user.id)
   end
 
+  def create
+    redirect_to new_path_step_path and return unless user_signed_in?
+    step = Step.new(steps_params)
+    step.update_attributes(asset_id: params['<option value='], path_id: params[:path_id])
+    step.get_position
+    if step.save
+      redirect_to path_path params[:path_id]
+    else
+      render :new
+    end
+  end
+
   private
   def steps_params
     params.require(:step).permit(:body, :position)
--- app/models/step.rb
+++ app/models/step.rb
@@ -3,4 +3,15 @@ class Step < ActiveRecord::Base
   belongs_to :path
 
   validates :path_id, :asset_id, :body, presence: true
+
+  # before_validation :get_position
+
+  # private
+  def get_position
+    steps = Path.find(self.path_id).steps
+    if steps.length > 1
+      sorted_steps = steps.sort_by { |step| step.position }
+      self.position = sorted_steps[-2].position + 1
+    end
+  end
 end
