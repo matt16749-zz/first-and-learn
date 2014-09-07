1b28d9d74d7cadf2458d8983c28c5260701c9ccd
Matt Chan <apprentice@master-inf-4.local>
2014-09-06 20:14:19 -0700

    Remove depreciated rspec/autorun from rails_helper Add feature tests sign_up for User

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -7,6 +7,29 @@ feature 'Index page' do
   end
 end
 
+feature 'Sign up' do
+  scenario 'Shows the Sign Up page' do
+    visit '/users/sign_up'
+    expect(page).to have_content "Sign up"
+    expect(page).to have_content "Last name"
+    expect(page).to have_content "Email"
+    expect(page).to have_content "Password (8 characters minimum)"
+    expect(page).to have_content "Password confirmation"
+  end
+
+  scenario 'Signs up a User' do
+    user2 = create(:user)
+    visit '/users/sign_up'
+    fill_in "First name", with: user2.first_name
+    fill_in "Last name", with: user2.last_name
+    fill_in "Email", with: "user2@gmail.com"
+    fill_in "Password", with: user2.password
+    fill_in "Password confirmation", with: user2.password
+    click_button "Sign up"
+    expect(page).to have_content "Welcome! You have signed up successfully."
+  end
+end
+
 feature 'Sign in' do
   before(:each) do
     logout(:user)
