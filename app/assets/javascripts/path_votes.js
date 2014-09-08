a2bc3d067a90720badc8d08a4890da24368311cd
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-07 20:06:27 -0700

    Refactor path_vote js into MVC.

--- app/assets/javascripts/path_votes.js
+++ app/assets/javascripts/path_votes.js
@@ -1,20 +1,30 @@
 $(document).on("page:change", function(){
-  var pathVote = $(".path-vote");
-  var pathVoteState = pathVote.data("vote-state");
-  var pathVoteableId = pathVote.data("voteable-id");
-  var pathVoteableType = pathVote.data("voteable-type");
-
-  pathVote.click(function(){
-   $.ajax({
-        url: "/paths/"+ pathVoteableId + "/votes",
-        type: "POST",
-        data: {
-          voteState: pathVoteState,
-          voteableId: pathVoteableId,
-          voteableType: pathVoteableType
-        }
-    }).done(function(data){
-      console.log(data)
-    });
-  });
+  var voteOpts = {
+    pathVote: '.path-vote',
+  };
+  var pathVote = new PathVoteController(voteOpts);
 });
+
+var PathVoteController = function (opts) {
+  this.pathVoteSelector = opts.pathVote;
+
+  this.addPathVoteListeners();
+};
+
+PathVoteController.prototype = {
+  addPathVoteListeners: function () {
+    $(this.pathVoteSelector).on('click', this.sendVote.bind(this));
+  },
+
+  sendVote: function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: "/paths/"+ e.target.dataset.voteableId + "/votes",
+      type: "POST",
+      data: e.target.dataset
+    })
+    .done(function(data){
+      $('#path-vote-count').html(data.vote_count);
+    });
+  }
+};
