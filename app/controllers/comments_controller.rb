710e2a3ca9ccc82eca465289b5fd7f01fc2d84cd
Patrick M. Menzel <pmmenzel@gmail.com>
2014-09-08 12:58:14 -0700

    Small refactor to the comments controller Use the merge function on the comment_params to add the user_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,8 +18,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    comment = @commentable.comments.build(comment_params)
-    comment.user_id = current_user.id
+    comment = @commentable.comments.build(comment_params.merge(user_id: current_user.id))
     if comment.save
       flash[:notice] = "Successfully created comment."
       redirect_to polymorphic_path(@commentable)
