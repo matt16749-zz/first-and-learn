24a0c5c2b820a2aa69443b48f7f0054ee0fa6bae
Matt <matt16749@gmail.com>
2014-09-09 15:41:07 -0700

    removes conflicting named route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "home#index"
 
-  devise_scope :user do 
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
-
   resources :users, except: [:new, :create, :destroy]
 
   resources :paths do
