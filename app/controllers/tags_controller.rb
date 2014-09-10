e7765947fe622316ef77b7a8fa27e361c90402a8
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-10 13:03:38 -0700

    Remove new action from tags controller. Update before_action callback to reflect removal of new action.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,6 @@
 class TagsController < ApplicationController
   include ApplicationHelper
-  before_action :redirect_to_sign_up, only: [:new, :create]
-
-  def new
-    @tag = Tag.new
-  end
+  before_action :redirect_to_sign_up, only: [:create]
 
   def create
     @tag = Tag.create(tags_params)
