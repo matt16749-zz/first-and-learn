91ab2c0faa9b8ddb2a43a77a7af87ecf2fcef081
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-10 14:54:01 -0700

    Correct association between tags, assets, and paths to be has_and_belongs_to_many instead of has_many.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
-  has_many :assets
-  has_many :paths
+  has_and_belongs_to_many :assets
+  has_and_belongs_to_many :paths
 
   validates :name, presence: true
   validates :name, uniqueness: true
