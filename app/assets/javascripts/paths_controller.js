2ce5d1a67e1e6113dfad1f219f5657f4eb9a4f4e
Gunnari Auvinen & Danielle Sublett <pair+cheerazar.daniellesublett@devbootcamp.com>
2014-09-09 17:08:45 -0700

    Add new function appendNewAsset.

--- app/assets/javascripts/paths_controller.js
+++ app/assets/javascripts/paths_controller.js
@@ -8,6 +8,17 @@ FirstAndLearn.PathsController.prototype = {
     $('[value="Save Asset"]').on('click', this.saveAsset.bind(this));
   },
 
+  appendNewAsset: function (asset) {
+    var newAssetHTML =  '<div style="display: none" class="asset">' +
+                        '<h3>' + asset.title +'</h3>' +
+                        '<button class="styled-button"><a href="/assets/' + asset.id + '">Details</a></button>' + " " +
+                        '<button class="styled-button"><a href="' + asset.url + '">View Asset</a></button>' +
+                        '<p>Description: ' + asset.description  + '</p>' +
+                        '</div>';
+    $('#asset-list').prepend(newAssetHTML);
+    $('#asset-list div:first').fadeIn('slow');
+  },
+
   clearAssetForm: function (asset) {
     $('#assets-partial').fadeOut('fast', function () {
       $('#asset_title').val('');
