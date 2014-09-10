76c3526afb0dba7d13f70d9de716ab58ad7460a5
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-10 11:07:20 -0700

    Create migration to change asset_type to category in assets table. Run db:migrate to update schema files.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910000305) do
+ActiveRecord::Schema.define(version: 20140910174648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140910000305) do
     t.integer  "user_id",                          null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "asset_type",  default: "tutorial"
+    t.string   "category",    default: "tutorial"
   end
 
   create_table "assets_tags", force: true do |t|
