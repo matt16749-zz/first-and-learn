5e57d57284776e70ac2c175153bf6f379d89f378
Matt <matt16749@gmail.com>
2014-09-10 09:45:37 -0700

    removes whitespace

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ require 'capybara/rspec'
 require 'factory_girl_rails'
 require 'database_cleaner'
 
-
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
