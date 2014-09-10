5ab073f75bf1c6fbad8fa6a2c2068521494c6c2c
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-10 12:58:32 -0700

    Create addTagsListener and invoke in js tags controller.

--- app/assets/javascripts/tags_controller.js
+++ app/assets/javascripts/tags_controller.js
@@ -1,6 +1,11 @@
 FirstAndLearn.TagsController = function () {
+  this.addTagsListeners();
 };
 
 FirstAndLearn.TagsController.prototype = {
+  addTagsListeners: function () {
+    $('.create-tags-button').on('click', this.showTagCreationInput.bind(this));
+    $('#add-tags').on('click', this.getTagsFromInputAndCreate.bind(this));
+  },
 
 };
