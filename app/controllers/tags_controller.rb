6be44d9eb0cbb3e06579f923e247bfd734fd7698
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-10 13:25:18 -0700

    Update tags create controller action.   - Can create multiple tags at once.   - Returns tag names in json.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,12 +3,18 @@ class TagsController < ApplicationController
   before_action :redirect_to_sign_up, only: [:create]
 
   def create
-    @tag = Tag.create(tags_params)
-    render :new
+    all_tags = tags_params[:name].split(/,\s*/)
+    tags_to_create = tags_that_need_to_be_created(all_tags)
+    tags_to_create.each { |tag| Tag.create(name: tag) }
+    render json: { tags: all_tags }
   end
 
   private
   def tags_params
     params.require(:tag).permit(:name)
+  end
+
+  def tags_that_need_to_be_created(tags_to_test)
+    tags_to_test.select { |tag| Tag.find_by_name(tag) == nil }
   end
 end
