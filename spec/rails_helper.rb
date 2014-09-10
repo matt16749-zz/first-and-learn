cdfeef187a9f498844ebed07089a01e6e75f63b1
Matt <matt16749@gmail.com>
2014-09-10 09:38:31 -0700

    adds code-climate integration commands to spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'capybara/rspec'
 require 'factory_girl_rails'
 require 'database_cleaner'
 
+
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
