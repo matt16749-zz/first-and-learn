e8d5edbe7cd1c3863700c90bc0ed50ee6f884a95
Yen Tran <yentran@Yens-MacBook-Pro.local>
2014-09-10 16:42:04 -0700

    Unit test for Comment Test Comments controller 	* "POST #create" 		- 'should increase path comment count in db by 1' 		- 'should increase asset comment count in db by 1'

--- spec/controllers/comments_spec.rb
+++ spec/controllers/comments_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe CommentsController, :type => :controller do
   let(:asset) { create(:asset, user_id: user.id) }
   let(:comment) {create(:comment, user_id: user.id, commentable_type:"Path")}
 
+  before :each do
+    User.destroy_all
+    Asset.destroy_all
+  end
+
   describe 'GET #show' do 
     it 'responds successfully with an HTTP 200 status code for path comments' do 
       get :show, path_id: path.id, id: comment.id
@@ -42,6 +47,16 @@ RSpec.describe CommentsController, :type => :controller do
       sign_in :user, user
       post :create, comment: FactoryGirl.attributes_for(:comment), asset_id: asset.id, commentable_type: "Path"
       expect(response).to have_http_status(302)
+    end
+
+    it 'should increase path comment count in db by 1' do
+      sign_in :user, user
+      expect { post :create, comment: FactoryGirl.attributes_for(:comment), path_id: path.id, commentable_type: "Path" }.to change{Comment.count}
+    end
+
+    it 'should increase asset comment count in db by 1' do
+      sign_in :user, user
+      expect { post :create, comment: FactoryGirl.attributes_for(:comment), asset_id: asset.id, commentable_type: "Path" }.to change{Comment.count}
     end
   end
 end
