7b823278b9cccefb2b99e050543b1a9d1ebdd8d8
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-10 19:16:58 -0700

    Remove tags new action test since the new action has been removed.

--- spec/controllers/tags_spec.rb
+++ spec/controllers/tags_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 RSpec.describe TagsController, :type => :controller do
   let(:user) { create(:user) }
 
-  describe "GET #new" do
-    it 'responds successfully with an HTTP 200 status code' do
-      sign_in :user, user
-      get :new
-      expect(response).to have_http_status(200)
-    end
-  end
-
   describe "POST #create" do
     before(:each) do
     sign_in :user, user
