9fbef8c3e0c2a858b3a4719086433498b7802935
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-10 19:48:22 -0700

    Refactor Tags controller test for spacing and syntax.

--- spec/controllers/tags_spec.rb
+++ spec/controllers/tags_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 RSpec.describe TagsController, :type => :controller do
   let(:user) { create(:user) }
 
-  describe "POST #create" do
-    before(:each) do
+  before(:each) do
     sign_in :user, user
-    post :create, tag: FactoryGirl.attributes_for(:tag)
-    end
+  end
+
+  describe "POST #create" do
     it "creates a new tag object" do
-      expect(Tag.all.count).to eq(1)
+      expect {
+        post :create, tag: FactoryGirl.attributes_for(:tag)
+      }.to change{Tag.count}.by(1)
     end
  end
 
